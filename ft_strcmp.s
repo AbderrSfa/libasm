f426bceccef350d3f23155c4523fb98a10b65955
Abderrahmane Sfaihi <asfaihi@e2r4p1.1337.ma>
2021-03-10 13:04:15 +0100

    Added ft_strcmp

---
+++ ft_strcmp.s
@@ -0,0 +1,35 @@
+;int		ft_strcmp(const char *s1, const char *s2);
+;return = rax
+;s1 = rdi
+;s2 = rsi
+
+			section		.text
+			global		_ft_strcmp
+_ft_strcmp:
+			mov rax, 0
+			mov dl, [rdi]
+			mov cl, [rsi]
+			inc rdi
+			inc rsi
+			cmp dl, 0
+			je _return
+			cmp cl, 0
+			je _return
+			cmp dl, cl
+			jl _lesser
+			jg _greater
+			je _ft_strcmp
+
+_return:
+			cmp dl, cl
+			jl _lesser
+			jg _greater
+			ret
+
+_greater:
+			mov rax, 1
+			ret
+
+_lesser:
+			mov rax, -1
+			ret
