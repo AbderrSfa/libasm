c25c114444ee090b81a83dd11e32ccb03c72a486
Abderrahmane Sfaihi <asfaihi@e2r4p1.1337.ma>
2021-03-16 18:48:15 +0100

    Tiny change in ft_strcpy

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -12,7 +12,7 @@ _ft_strcpy:	mov rcx, 0
 
 looper:		mov dl, BYTE [rsi + rcx]
 			mov BYTE [rdi + rcx], dl
-			cmp BYTE [rsi + rcx], 0
+			cmp dl, 0
 			je exit
 			inc rcx
 			jmp looper
--- main.c
+++ main.c
@@ -1,22 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/15 17:20:56 by asfaihi           #+#    #+#             */
+/*   Updated: 2021/03/16 15:06:16 by asfaihi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-#define TEST1 "fqwfqwfqwfqqwf"
-#define TEST2 "Hello"
-
 size_t	ft_strlen(const char *s);
-char	*ft_strcpy(char * dst, const char * src);
+char	*ft_strcpy(char *dst, const char *src);
 int		ft_strcmp(const char *s1, const char *s2);
 ssize_t	ft_write(int fildes, const void *buf, size_t nbyte);
 ssize_t	ft_read(int fildes, void *buf, size_t nbyte);
+char	*ft_strdup(const char *s1);
 
-char *	ft_strdup(const char *s1);
-
-// error when file doesn't exist
-
-int main(int argc, char const *argv[])
+int		main()
 {
 	char	*a;
 	char	b[80];
@@ -27,16 +33,16 @@ int main(int argc, char const *argv[])
 	fd2 = open("trry.txt", O_RDWR);
 
 	printf("ft_strlen:\n");
-	printf("MINE:\t\t|%lu|\n", ft_strlen(TEST1));
-	printf("ORIGINAL:\t|%lu|\n", strlen(TEST1));
+	printf("MINE:\t\t|%lu|\n", ft_strlen("Hello"));
+	printf("ORIGINAL:\t|%lu|\n", strlen("Hello"));
 
 	printf("\nft_strcpy:\n");
-	printf("MINE:\t\t|%s|\n", ft_strcpy(b, TEST1));
-	printf("ORIGINAL:\t|%s|\n", strcpy(b, TEST1));
+	printf("MINE:\t\t|%s|\n", ft_strcpy(b, "Hello"));
+	printf("ORIGINAL:\t|%s|\n", strcpy(b, "Hello"));
 
 	printf("\nft_strcmp\n");
-	printf("MINE:\t\t|%d|\n", ft_strcmp(TEST1, TEST2));
-	printf("ORIGINAL:\t|%d|\n", strcmp(TEST1, TEST2));
+	printf("MINE:\t\t|%d|\n", ft_strcmp("Hello", "Hello"));
+	printf("ORIGINAL:\t|%d|\n", strcmp("Hello", "Hello"));
 
 	printf("\nft_write\n");
 	printf("MINE:\t\t|%ld|\n", ft_write(fd, "Yeeeeeee", 5));
@@ -47,7 +53,7 @@ int main(int argc, char const *argv[])
 	printf("ORIGINAL:\t|%ld|\n", read(fd2, b, 5));
 
 	printf("\nft_strdup\n");	
-	printf("MINE:\t\t|%s|\n", a = ft_strdup(TEST1));
-	printf("ORIGINAL:\t|%s|\n", strdup(TEST1));
+	printf("MINE:\t\t|%s|\n", a = ft_strdup("Hello"));
+	printf("ORIGINAL:\t|%s|\n", a = strdup("Hello"));
 	return (0);
 }
