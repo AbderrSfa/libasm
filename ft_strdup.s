97ea4d52c27b701d1120b4de967ea030e87c3a28
Abderrahmane Sfaihi <asfaihi@e2r4p1.1337.ma>
2021-03-16 14:38:36 +0100

    Calling ft_strlen and ft_strcpy externally

--- ft_strdup.s
+++ ft_strdup.s
@@ -4,40 +4,19 @@
 
 global		_ft_strdup
 extern 		_malloc
+extern		_ft_strlen
+extern		_ft_strcpy
 
 section		.text
 
 _ft_strdup:	push rdi
-			call ft_strlen
+			call _ft_strlen
 
 			mov rdi, rax
 			call _malloc
 
 			pop rsi
 			mov rdi, rax
-			call ft_strcpy
+			call _ft_strcpy
 
-			ret
-
-ft_strcpy:	mov rcx, 0
-			jmp looper2
-
-looper2:	mov dl, BYTE [rsi + rcx]
-			mov BYTE [rdi + rcx], dl
-			cmp BYTE [rsi + rcx], 0
-			je exit
-			inc rcx
-			jmp looper2
-
-exit:		mov rax, rdi
-			ret
-
-ft_strlen:	mov rax, -1
-			jmp looper
-
-looper:		mov cl, [rdi]
-			inc rdi
-			inc rax
-			cmp cl, 0
-			jne looper
 			ret
