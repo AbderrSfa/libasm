47f9e87e5d664755a3e398a567ed29b0f0858274
Abderrahmane Sfaihi <asfaihi@e2r4p1.1337.ma>
2021-03-18 11:34:42 +0100

    change in main.c

--- main.c
+++ main.c
@@ -18,25 +18,19 @@ int		main()
 	printf("ft_strlen:\n");
 	printf("MINE:\t\t|%lu|\n", ft_strlen("Hello"));
 	printf("ORIGINAL:\t|%lu|\n", strlen("Hello"));
-
 	printf("\nft_strcpy:\n");
 	printf("MINE:\t\t|%s|\n", ft_strcpy(b, "Hello"));
 	printf("ORIGINAL:\t|%s|\n", strcpy(b, "Hello"));
-
 	printf("\nft_strcmp\n");
 	printf("MINE:\t\t|%d|\n", ft_strcmp("Hello", "Hello"));
 	printf("ORIGINAL:\t|%d|\n", strcmp("Hello", "Hello"));
-
 	printf("\nft_write\n");
 	printf("MINE:\t\t|%ld|\n", ft_write(1, "Yeeeeeee", 5));
 	printf("ORIGINAL:\t|%ld|\n", write(1, "Yeeeeeee", 5));
-
 	printf("\nft_read\n");
 	printf("MINE:\t\t|%ld|\n", ft_read(0, b, 5));
 	printf("ORIGINAL:\t|%ld|\n", read(0, b, 5));
-
 	printf("\nft_strdup\n");	
 	printf("MINE:\t\t|%s|\n", a = ft_strdup("Hello"));
 	printf("ORIGINAL:\t|%s|\n", a = strdup("Hello"));
-	return (0);
 }
