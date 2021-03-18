18aa749e36f57c70cc1b9585bb811be26d708a9d
Abderrahmane Sfaihi <asfaihi@e2r4p1.1337.ma>
2021-03-18 11:30:37 +0100

    change in main.c

--- main.c
+++ main.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/15 17:20:56 by asfaihi           #+#    #+#             */
-/*   Updated: 2021/03/16 15:06:16 by asfaihi          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -26,11 +14,6 @@ int		main()
 {
 	char	*a;
 	char	b[80];
-	int		fd;
-	int		fd2;
-
-	fd = open("trry.txt", O_RDWR);
-	fd2 = open("trry.txt", O_RDWR);
 
 	printf("ft_strlen:\n");
 	printf("MINE:\t\t|%lu|\n", ft_strlen("Hello"));
@@ -45,12 +28,12 @@ int		main()
 	printf("ORIGINAL:\t|%d|\n", strcmp("Hello", "Hello"));
 
 	printf("\nft_write\n");
-	printf("MINE:\t\t|%ld|\n", ft_write(fd, "Yeeeeeee", 5));
-	printf("ORIGINAL:\t|%ld|\n", write(fd2, "Yeeeeeee", 5));
+	printf("MINE:\t\t|%ld|\n", ft_write(1, "Yeeeeeee", 5));
+	printf("ORIGINAL:\t|%ld|\n", write(1, "Yeeeeeee", 5));
 
 	printf("\nft_read\n");
-	printf("MINE:\t\t|%ld|\n", ft_read(fd, b, 5));
-	printf("ORIGINAL:\t|%ld|\n", read(fd2, b, 5));
+	printf("MINE:\t\t|%ld|\n", ft_read(0, b, 5));
+	printf("ORIGINAL:\t|%ld|\n", read(0, b, 5));
 
 	printf("\nft_strdup\n");	
 	printf("MINE:\t\t|%s|\n", a = ft_strdup("Hello"));
